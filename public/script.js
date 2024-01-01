f7df2d1091bb599d3cc640645a93aa6655450380
Domminic Mayer <domminicm@nevada.unr.edu>
2023-12-31 20:04:29 -0800

    adding more content

--- public/script.js
+++ public/script.js
@@ -1,7 +1,2 @@
 document.addEventListener("DOMContentLoaded", function() {
 });
-
-document.getElementById('contactFormButton').addEventListener('click', function() {
-    var form = document.getElementById('contactFormPopOut');
-    form.style.display = form.style.display === 'block' ? 'none' : 'block';
-});  
